dca9a87ccc631a7ee3d5a53492278051e4989d57
khil94 <zidru94@gmail.com>
2024-04-07 23:20:40 +0900

    [feat] : HeaderSection 카테고리 기능

--- src/Section/HeaderSection.tsx
+++ src/Section/HeaderSection.tsx
@@ -1,17 +1,31 @@
 import { useDispatch, useSelector } from "react-redux";
 import { CATEGORIES } from "../Constants/Enum";
-import { RootState } from "../Store/CategoryStore";
+import { RootState, selector } from "../Store/CategoryStore";
 import "./HeaderSection.scss";
 
 export default function HeaderSection() {
   const category = useSelector((state: RootState) => state.category.value);
   const dispatch = useDispatch();
 
+  function categoryHandler(idx: number) {
+    dispatch(selector(idx));
+  }
+
   return (
     <header id="header">
       <div className="header_wrapper">
         {CATEGORIES.map((v, i) => {
-          return <div></div>;
+          return (
+            <div
+              className={`header_category ${category === i ? "selected" : ""}`}
+              key={"header_category_" + v.name}
+              onClick={() => {
+                categoryHandler(i);
+              }}
+            >
+              {v.name}
+            </div>
+          );
         })}
       </div>
     </header>
