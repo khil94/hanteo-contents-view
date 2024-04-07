44c97bfabb7feaa60af7b2349c987afb3f8f52bb
khil94 <zidru94@gmail.com>
2024-04-07 15:27:08 +0900

    [feat] : first commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import "./App.css";
+import ContentList from "./Section/ContentListSection";
+import Footer from "./Section/FooterSection";
+import Header from "./Section/HeaderSection";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <ContentList />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Constants/Enum.ts
@@ -0,0 +1,22 @@
+import { ICategory } from "../types/types";
+
+export const CATEGORIES: ReadonlyArray<ICategory> = [
+  {
+    name: "차트",
+  },
+  {
+    name: "Whook",
+  },
+  {
+    name: "이벤트",
+  },
+  {
+    name: "뉴스",
+  },
+  {
+    name: "스토어",
+  },
+  {
+    name: "충전소",
+  },
+];
