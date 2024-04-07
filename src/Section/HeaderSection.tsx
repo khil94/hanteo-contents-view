f0d0ed14fcd4d3f45ae1803c67452c45cd91baae
khil94 <zidru94@gmail.com>
2024-04-07 22:24:43 +0900

    [feat] : HeaderSection 우선 기본 틀만

--- src/Section/HeaderSection.tsx
+++ src/Section/HeaderSection.tsx
@@ -1,3 +1,19 @@
+import { useDispatch, useSelector } from "react-redux";
+import { CATEGORIES } from "../Constants/Enum";
+import { RootState } from "../Store/CategoryStore";
+import "./HeaderSection.scss";
+
 export default function HeaderSection() {
-  return <></>;
+  const category = useSelector((state: RootState) => state.category.value);
+  const dispatch = useDispatch();
+
+  return (
+    <header id="header">
+      <div className="header_wrapper">
+        {CATEGORIES.map((v, i) => {
+          return <div></div>;
+        })}
+      </div>
+    </header>
+  );
 }
