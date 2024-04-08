e8b63bf219c3a290c3d2539d8d2b2e9a1c1da8c6
khil94 <zidru94@gmail.com>
2024-04-08 20:10:18 +0900

    [feat] : Banner Carousel 적용

--- src/Components/Banner.tsx
+++ src/Components/Banner.tsx
@@ -3,14 +3,26 @@ import "./Banner.scss";
 
 export default function Banner(data: IBanner) {
   return (
-    <div className="banner_wrapper">
-      <a href={data.url}>
-        <img height={160} className="banner_img" src={data.imgUrl} />
-      </a>
-      <div
-        className={`banner_processing ${data.isInProgress ? "" : "expired"}`}
-      >
-        {data.isInProgress ? "진행 중" : "종료"}
+    <div className="banner_outer_wrapper">
+      <div className="banner_wrapper">
+        <a
+          href={data.url}
+          onClick={(e) => {
+            e.preventDefault();
+          }}
+        >
+          <img
+            height={140}
+            className="banner_img"
+            src={data.imgUrl}
+            alt={data.imgUrl}
+          />
+        </a>
+        <div
+          className={`banner_processing ${data.isInProgress ? "" : "expired"}`}
+        >
+          {data.isInProgress ? "진행 중" : "종료"}
+        </div>
       </div>
     </div>
   );
--- src/Section/BannerSection.tsx
+++ src/Section/BannerSection.tsx
@@ -1,15 +1,72 @@
+import { useEffect, useRef, useState } from "react";
 import Banner from "../Components/Banner";
 import { BANNERS } from "../Constants/Enum";
+import { IBanner } from "../types/types";
 import "./BannerSection.scss";
 
 export default function BannerSection() {
+  const [idx, setIdx] = useState(1);
+  const wrapper = useRef<HTMLDivElement>(null);
+  const [data, setData] = useState<ReadonlyArray<IBanner>>(BANNERS);
+
+  useEffect(() => {
+    if (data.length !== 0) {
+      setData([BANNERS[BANNERS.length - 1], ...BANNERS, BANNERS[0]]);
+    }
+  }, [BANNERS]);
+
+  useEffect(() => {
+    const autoSlide = setInterval(() => {
+      console.log("timeout run");
+      handleSlide(1);
+    }, 3000);
+
+    return () => clearTimeout(autoSlide);
+  });
+
+  useEffect(() => {
+    if (wrapper.current !== null) {
+      wrapper.current.style.transform = `translateX(-${idx}00%)`;
+    }
+  }, [idx]);
+
+  console.log(idx);
+
+  function moveCarousel(newIdx: number) {
+    setTimeout(() => {
+      setIdx(newIdx);
+      if (wrapper.current !== null) {
+        wrapper.current.style.transition = "";
+      }
+    }, 500);
+  }
+
+  function handleSlide(direction: number) {
+    const newIdx = idx + direction;
+    if (newIdx === BANNERS.length + 1) {
+      moveCarousel(1);
+    } else if (newIdx === 0) {
+      moveCarousel(BANNERS.length);
+    }
+
+    setIdx((prev) => {
+      console.log("added", newIdx);
+      return prev + direction;
+    });
+
+    if (wrapper.current !== null) {
+      wrapper.current.style.transition = "all 0.5s ease-in-out";
+    }
+  }
+
   return (
     <div className="banner_section_wrapper">
-      <div className="banner_list_wrapper">
-        {BANNERS.map((v) => {
+      <div className="banner_list_wrapper" ref={wrapper}>
+        {data.map((v) => {
           return <Banner {...v} />;
         })}
       </div>
+      <div className="banner_position_wrapper"></div>
     </div>
   );
 }
