0f385c707a53b3ce1dc1a49a737d668395e40088
khil94 <zidru94@gmail.com>
2024-04-09 21:15:14 +0900

    [feat] : 카테고리 enum값에 url추가

--- src/Constants/Enum.ts
+++ src/Constants/Enum.ts
@@ -3,38 +3,44 @@ import { IBanner, ICategory } from "../types/types";
 export const CATEGORIES: ReadonlyArray<ICategory> = [
   {
     name: "차트",
+    url: "Chart",
   },
   {
     name: "Whook",
+    url: "Whook",
   },
   {
     name: "이벤트",
+    url: "Event",
   },
   {
     name: "뉴스",
+    url: "News",
   },
   {
     name: "스토어",
+    url: "Store",
   },
   {
     name: "충전소",
+    url: "Charge",
   },
 ];
 
 export const BANNERS: ReadonlyArray<IBanner> = [
   {
     url: "",
-    imgUrl: "",
+    imgUrl: "1",
     isInProgress: true,
   },
   {
     url: "",
-    imgUrl: "",
+    imgUrl: "2",
     isInProgress: false,
   },
   {
     url: "",
-    imgUrl: "",
+    imgUrl: "3",
     isInProgress: true,
   },
 ];
