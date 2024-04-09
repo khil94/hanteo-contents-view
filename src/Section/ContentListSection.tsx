179a54f5afe44325fd465c957c62542d0a379da3
khil94 <zidru94@gmail.com>
2024-04-09 21:16:55 +0900

    [feat] : ContentListSection 무한 스크롤링 임시 구현

--- src/Section/ContentListSection.tsx
+++ src/Section/ContentListSection.tsx
@@ -1,16 +1,38 @@
-import { useRef } from "react";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import Content from "../Components/Content";
-import { CATEGORIES } from "../Constants/Enum";
-import { MOCKUP_LIST } from "../Constants/Mockup";
-import { RootState, selector } from "../Store/CategoryStore";
+import Content from "../components/Content";
+import { CATEGORIES } from "../constants/Enum";
+import { useIntersectionObserver } from "../hooks/useIntersectionObserver";
+import { RootState, selector } from "../store/CategoryStore";
+import { IMockupData } from "../types/types";
 import "./ContentListSection.scss";
 
 export default function ContentListSection() {
-  const currPage = useRef(0);
+  const [currPage, setCurrPage] = useState(-1);
+  const [data, setData] = useState<IMockupData[]>([]);
 
+  useIntersectionObserver("#pivot", () => {
+    setCurrPage((prev) => prev + 1);
+  });
   const category = useSelector((state: RootState) => state.category.value);
   const dispatch = useDispatch();
+
+  const getMockData = (url: string, page: number) => {
+    fetch(`http://localhost:5173/${url}Mockup.json`)
+      .then((res) => res.json())
+      .then((target) => {
+        console.log(
+          "target",
+          currPage,
+          target.slice(page * 20, (page + 1) * 20)
+        );
+        setData([...data, ...target.slice(page * 10, (page + 1) * 10)]);
+      });
+  };
+
+  useEffect(() => {
+    getMockData(CATEGORIES[category].url, currPage);
+  }, [currPage]);
 
   function categoryHandler(idx: number) {
     dispatch(selector(idx));
@@ -19,10 +41,11 @@ export default function ContentListSection() {
     <div className="content_list_section_wrapper">
       {CATEGORIES[category].name}
       <div className="content_list_wrapper">
-        {MOCKUP_LIST.map((v) => {
-          return <Content {...v} />;
+        {data.map((v, i) => {
+          return <Content key={`content_${v.title}_${i}`} {...v} />;
         })}
       </div>
+      <div id="pivot"></div>
     </div>
   );
 }
