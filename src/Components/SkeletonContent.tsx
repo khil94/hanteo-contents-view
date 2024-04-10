a44f95c7dfcf120abc82a74b68ea6145823df812
khil94 <zidru94@gmail.com>
2024-04-10 23:00:55 +0900

    [style] : SkeletonContent 이미지칸 div로 수정

--- src/Components/SkeletonContent.tsx
+++ src/Components/SkeletonContent.tsx
@@ -4,7 +4,7 @@ export default function SkeletonContent() {
   return (
     <div className="skeleton_content_wrapper">
       <a className="skeleton_content">
-        <img className="skeleton_content_img" width={48} />
+        <div className="skeleton_content_img" />
         <div className="skeleton_content_title" />
       </a>
     </div>
